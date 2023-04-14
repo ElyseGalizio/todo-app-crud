8c34adee3de21c6aeb94d544c34c054b0d50a0a3
Elyse Galizio <elyse.galizio@gmail.com>
2023-04-14 10:05:20 +0200

    completed filter function

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,9 +1,13 @@
+import './ToDo.css';
+
 export default function ToDo(props) {
     return (
-        <div>
-            <p>{props.title}</p>
+        <div className="todo">
+            <p className={props.completed ? 'strikethrough' : 'original'}>{props.title}</p>
+            {props.completed ? <p>completed</p> : <p>still in progress</p>}
+            <input checked={props.completed} type='checkbox' onChange={(event) => props.editToDo(event, props.id)} />
             {/* we are lifting up the state */}
-            <button onClick={() => props.deleteToDo(props.id)}>Delete</button>
+            <button className='todo-delete-button' onClick={() => props.deleteToDo(props.id)}>Delete</button>
         </div>
     )
 }
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 import ToDo from "../ToDo/ToDo";
+import './ToDoList.css';
 
 const defaultTodo = {
     title: '',
-    date: ''
 }
 
 export default function ToDoList() {
     const [newTodo, setNewTodo] = React.useState(defaultTodo)
+    const [filterTodosValue, setFilterTodosValue] = React.useState('show-all')
+
     const [todos, setTodos] = React.useState([
         {
             title: "wash dishes",
@@ -17,6 +19,7 @@ export default function ToDoList() {
         },
         {
             title: "vacuum floors",
+            completed: false,
             id: 2
         }
     ])
@@ -39,15 +42,48 @@ export default function ToDoList() {
         setTodos(newTodos)
         setNewTodo(defaultTodo)
     }
+
+    //edit todo by toggling the completion using a checkbox
+    const editToDo = (event, todoToBeEditedId) => {
+        const isTodoCompleted = event.target.checked
+        const newTodos = todos.map(item => {
+            if (item.id === todoToBeEditedId) {
+                return { ...item, completed: isTodoCompleted }
+            }
+            return item
+        })
+        setTodos(newTodos)
+    }
+
+    function filterTodos(todos, filterQuery) {
+        if (filterQuery === 'completed') {
+            return todos.filter(item => item.completed !== false)
+        } else if (filterQuery === 'in-progress') {
+            return todos.filter(item => item.completed === false)
+        } else if (filterQuery === 'show-all') {
+            return todos
+        }
+    }
+    //deploying the project to netlify
+    //adding in some conditional styling
+
+    // select has multiple options, we need to store what the user selected (state)
+    // where the todos are mapped, we can add a filter prior to the map to ony show either
+    // completed or in progress
+
     return (
         <div>
             <h1>ToDo List</h1>
-            <form onSubmit={createNewTodo}>
+            <select onChange={(event) => setFilterTodosValue(event.target.value)}>
+                <option value="show-all">All Todos</option>
+                <option value="completed">Completed</option>
+                <option value="in-progress">In Progress</option>
+            </select>
+            <form onSubmit={createNewTodo} className="todolist-form">
                 <input onChange={handleChange} type="text" name="title" value={newTodo.title} placeholder="type your todo" />
-                <input onChange={handleChange} type="text" name="date" value={newTodo.date} placeholder="type your todo" />
-                <button>Create Todo</button>
+                <button className="todolist-button">Create Todo</button>
             </form>
-            {todos.map(item => <ToDo {...item} deleteToDo={deleteToDo} />)}
+            {filterTodos(todos, filterTodosValue).map(item => <ToDo key={item.id} {...item} deleteToDo={deleteToDo} editToDo={editToDo} />)}
         </div>
     )
 }
